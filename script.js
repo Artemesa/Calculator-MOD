4a2a07254b15b05b75f8c1e444c59152778b6620
Daniel <daniel.j.sotirov@gmail.com>
2023-02-07 16:20:44 +0200

    nachaloto na calculatora 2 chisla i otgovor

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+let a = document.getElementById("inputA").value;
+let b = document.getElementById("inputB").value;
+let result = document.getElementById("result");
+a = Number(a);
+b = Number(b);
+let c = a + b;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(result);
+result.innerText = c;
